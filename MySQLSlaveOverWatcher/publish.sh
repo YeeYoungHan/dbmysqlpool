688b3d88104122935590d83723c377cc458756be
Yee Young Han <websearch@naver.com>
2018-11-15 15:38:06 +0900

    tar.gz 파일 삭제후, 생성한다.

--- MySQLSlaveOverWatcher/publish.sh
+++ MySQLSlaveOverWatcher/publish.sh
@@ -5,5 +5,6 @@ version=`echo $version | awk 'BEGIN {FS="[-]"}{print $2}'`
 bit=`getconf LONG_BIT`
 bit+="bit"
 filename="MySQLSlaveOverWatcher-$version-cent6.3-$bit.tar.gz"
+rm -f $filename
 tar czvf $filename MySQLSlaveOverWatcher.exe MySQLSlaveOverWatcher.xml mysqlslaveoverwatcher.sh
 
